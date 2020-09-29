7c1534fdc099a25565c7715b2b40583b6c8be386
Michael P Garrity <mike@mikegslptop2495.lan>
2020-09-29 09:53:41 -0400

    Updates

---
+++ precache-manifest.1a8c2c224dd22bd667aab7f7ea55287f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d78497f9f51578c187f4f3da91c9612",
+    "url": "/gaussian_primes/index.html"
+  },
+  {
+    "revision": "1be6b21dd02d130ce197",
+    "url": "/gaussian_primes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ddd55dad154dde3f90f9",
+    "url": "/gaussian_primes/static/js/2.f997fcb7.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/gaussian_primes/static/js/2.f997fcb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be6b21dd02d130ce197",
+    "url": "/gaussian_primes/static/js/main.b9a45306.chunk.js"
+  },
+  {
+    "revision": "5c1a516d0fc51ffd767e",
+    "url": "/gaussian_primes/static/js/runtime-main.358a3728.js"
+  }
+]);
